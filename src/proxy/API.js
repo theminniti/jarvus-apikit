da80aa9373da50b73bd55fa14b5fa7b2799a1f85
Chris Alfano <chris@jarv.us>
2015-07-28 01:07:35 -0400

    drop down to extending proxy.Server to do away with proxy.Ajax's belligerent doRequest and mostly useless features

--- src/proxy/API.js
+++ src/proxy/API.js
@@ -1,6 +1,10 @@
 /*jslint browser: true, undef: true *//*global Ext*/
+/**
+ * TODO:
+ * - Just extend Server and deprecated the other stuff Ajax does? callParent for buildrequest?
+ */
 Ext.define('Jarvus.proxy.API', {
-    extend: 'Ext.data.proxy.Ajax',
+    extend: 'Ext.data.proxy.Server',
     alias: 'proxy.api',
     requires: [
         'Jarvus.util.API'
@@ -11,9 +15,14 @@ Ext.define('Jarvus.proxy.API', {
          * @cfg The {Ext.data.Connection} instance that will process requests
          * @required
          */
-        connection: 'Jarvus.util.API'
+        connection: 'Jarvus.util.API',
+
+        headers: null
     },
 
+    /**
+     * Automatically inject "connection" class into requires
+     */
     onClassExtended: function(cls, data, hooks) {
         var connection = data.connection || data.config && data.config.connection,
             onBeforeClassCreated;
@@ -32,6 +41,9 @@ Ext.define('Jarvus.proxy.API', {
         }
     }, 
 
+    /**
+     * Convert "connection" class into constructor reference
+     */
     applyConnection: function(connection) {
         if (typeof connection == 'string') {
             Ext.syncRequire(connection);
@@ -41,17 +53,66 @@ Ext.define('Jarvus.proxy.API', {
         return connection;
     },
 
+    getMethod: function(request) {
+        switch (request.getAction()) {
+            case 'create':
+                return 'POST';
+            case 'read':
+                return 'GET';
+            case 'update':
+                return 'PATCH';
+            case 'destroy':
+                return 'DELETE';
+        }
+    },
+
+    buildRequest: function(operation) {
+        var me = this,
+            request = me.callParent(arguments);
+
+        request.setMethod(me.getMethod(request));
+        request.setHeaders(me.getHeaders(request));
+        request.setTimeout(me.getTimeout(request));
+
+        return request;
+    },
+
+    doRequest: function(operation) {
+        var me = this,
+            writer = me.getWriter(),
+            request = me.buildRequest(operation);;
+
+        if (writer && operation.allowWrite()) {
+            request = writer.write(request);
+        }
+
+        request.setCallback(function(options, success, response) {
+            if (request === me.lastRequest) {
+                me.lastRequest = null;
+            }
+
+            me.processResponse(success, operation, request, response);
+        });
+
+        return me.sendRequest(request);
+    },
+
     sendRequest: function(request) {
         var me = this;
 
         request.setRawRequest(me.getConnection().request(Ext.applyIf({
             autoDecode: false,
-            disableCaching: false,
             failureStatusCodes: [404] // TODO: verify this results in the proper failure method being called in the proxy
         }, request.getCurrentConfig())));
 
         me.lastRequest = request;
 
         return request;
+    },
+
+    destroy: function() {
+        this.lastRequest = null;
+        
+        this.callParent();
     }
 });
